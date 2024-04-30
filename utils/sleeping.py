0656c5b19ce6fb67c60340b7a504edbe7252b096
georgrin <80432064+georgrin@users.noreply.github.com>
2024-04-30 08:38:22 +0300

    Update sleeping.py

--- utils/sleeping.py
+++ utils/sleeping.py
@@ -5,45 +5,9 @@ from loguru import logger
 import asyncio
 import aioconsole
 
-import curses
-
-async def sleep(sleep_from, sleep_to):
-    delay = random.randint(sleep_from, sleep_to)
-    print(f"💤 Sleep {delay} s. Press Enter to interrupt.")
-
-    def handle_input(stdscr):
-        curses.noecho()  # Отключаем отображение вводимых символов
-        curses.cbreak()  # Режим немедленного чтения ввода
-        stdscr.keypad(True)  # Включаем поддержку специальных клавиш
-
-        while True:
-            key = stdscr.getch()
-            if key == 10:  # Код клавиши Enter
-                return True
-
-    async def sleep_task():
-        try:
-            for _ in range(delay):
-                await asyncio.sleep(1)
-        except asyncio.CancelledError:
-            print("Sleep interrupted!")
-
-    # Запускаем задачи одновременно
-    loop = asyncio.get_event_loop()
-    with curses.initscr() as stdscr:  # Инициализация ncurses
-        done, pending = await asyncio.wait(
-            [sleep_task(), loop.run_in_executor(None, handle_input, stdscr)],
-            return_when=asyncio.FIRST_COMPLETED
-        )
-
-    # Прерываем sleep_task, если она еще не завершена
-    for task in pending:
-        task.cancel()
-
-"""
 async def sleep(sleep_from, sleep_to, key='q'):
     delay = random.randint(sleep_from, sleep_to)
-    print(f"💤 Sleep {delay} s. Press '{key}' to interrupt.")
+    print(f"💤 Sleep {delay} s. Press '{key} and Enter' to interrupt.")
 
     async def wait_for_key():
         while True:
@@ -67,5 +31,3 @@ async def sleep(sleep_from, sleep_to, key='q'):
     # Прерываем sleep_task, если она еще не завершена
     for task in pending:
         task.cancel()
-
-"""
