02ff8eaf8ae8c7bdee5868fe3af380f069884b00
Ilya <>
2024-04-30 13:49:11 +0300

    py3.8 compatibility

--- utils/sleeping.py
+++ utils/sleeping.py
@@ -1,15 +1,17 @@
 import asyncio
 import random
 
-import aioconsole
+from getch import getch
+
 
 async def sleep(sleep_from, sleep_to, key='q'):
     delay = random.randint(sleep_from, sleep_to)
-    print(f"💤 Sleep {delay} s. Press '{key} and Enter' to interrupt.")
+    print(f"💤 Sleep {delay} s. Press '{key}' to interrupt.")
 
     async def wait_for_key():
+        loop = asyncio.get_running_loop()
         while True:
-            key_pressed = await aioconsole.ainput()
+            key_pressed = await loop.run_in_executor(None, getch)
             if key_pressed == key:
                 return True
 
