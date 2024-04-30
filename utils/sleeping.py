c3a10438f3b5e482f8981ca48daf2bad246def64
georgrin <80432064+georgrin@users.noreply.github.com>
2024-04-30 08:02:39 +0300

    Update sleeping.py

--- utils/sleeping.py
+++ utils/sleeping.py
@@ -3,10 +3,34 @@ import random
 
 from loguru import logger
 
+from pynput import keyboard
 
+async def sleep(sleep_from: int, sleep_to: int):
+    delay = random.randint(sleep_from, sleep_to)
+    logger.info(f"💤 Sleep {delay} s.")
+
+    def on_press(key):
+        if key == keyboard.Key.space:
+            # Stop listener
+            return False
+
+    # Start the keyboard listener
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
+
+    try:
+        for _ in range(delay):
+            await asyncio.sleep(1)
+            if not listener.is_alive():
+                break  # Прерываем sleep, если слушатель остановлен
+    finally:
+        listener.stop()  # Останавливаем слушатель в любом случае
+
+"""
 async def sleep(sleep_from: int, sleep_to: int):
     delay = random.randint(sleep_from, sleep_to)
 
     logger.info(f"💤 Sleep {delay} s.")
     for _ in range(delay):
         await asyncio.sleep(1)
+"""
