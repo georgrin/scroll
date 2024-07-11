7553efcf5083767a17b33109b884782cd326558c
georgrin <80432064+georgrin@users.noreply.github.com>
2024-07-11 23:46:21 +0300

    Update account.py

--- modules/account.py
+++ modules/account.py
@@ -189,6 +189,7 @@ class Account:
         if transaction.get("gasPrice", None) is None:
             max_priority_fee_per_gas = self.w3.to_wei(MAX_PRIORITY_FEE["ethereum"], "gwei")
             max_fee_per_gas = await self.w3.eth.gas_price
+            max_fee_per_gas = int(max_fee_per_gas * GAS_MULTIPLIER)
 
             transaction.update(
                 {
