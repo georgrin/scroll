6744f1063801e202812a74d2d8d10a0b26d39224
georgrin <80432064+georgrin@users.noreply.github.com>
2024-07-15 11:54:41 +0300

    Update rhomarkets.py

--- modules/rhomarkets.py
+++ modules/rhomarkets.py
@@ -97,11 +97,13 @@ class Rhomarkets(Account):
             )
 
             tx_data = await self.get_tx_data()
-            print(tx_data)
+
             transaction = await self.contract.functions.redeemUnderlying(
                 amount,
             ).build_transaction(tx_data)
 
+            transaction(tx_data)
+            
             signed_txn = await self.sign(transaction)
 
             txn_hash = await self.send_raw_transaction(signed_txn)
